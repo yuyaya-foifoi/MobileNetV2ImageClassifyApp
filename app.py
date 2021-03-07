385241bd2366ec806842409ad3a78b4999354d25
勇哉 藤崎 <yuyafujisaki@yuya-fujisaki-MacBookPro.local>
2021-03-07 15:53:52 +0900

    update model

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import os
 import glob
 import re
 import numpy as np
+import shutil
+import wikipedia
 
 # Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
 from keras.preprocessing import image
 
 # Flask utils
@@ -20,10 +20,11 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'models/kerasmobilenet.h5'
+MODEL_PATH = 'models/MobileNetV2.h5'
 
-from keras.applications.mobilenet import MobileNet
-model = MobileNet(input_shape=(224, 224,3), alpha=1.0, depth_multiplier=1, dropout=1e-3, include_top=True, weights=None, input_tensor=None, pooling=None, classes=1000)
+from keras.applications import MobileNetV2
+from keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+model = MobileNetV2(input_shape=(224, 224,3), alpha=1.0, include_top=True, weights=None, input_tensor=None, pooling=None, classes=1000)
 model.load_weights(MODEL_PATH)
 
 print('Model loaded. Check http://127.0.0.1:5000/')
@@ -34,12 +35,8 @@ def model_predict(img_path, model):
 
     # Preprocessing the image
     x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
+    x = preprocess_input(x)
 
     preds = model.predict(x)
     return preds
@@ -56,8 +53,6 @@ def upload():
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
-        
-        print(f)
 
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
@@ -67,20 +62,23 @@ def upload():
 
         # Make prediction
         preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
         pred_class = decode_predictions(preds, top=5)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        print(pred_class)
-        print(pred_class[0][0][1])
         results = []
         for i in [0, 1, 2, 3, 4]:
             results.append(str(pred_class[0][i][1]))
+            results.append(' : ')
+            results.append(str(pred_class[0][i][2]))
+            results.append(' , ')
             
-        return ", " .join(results)
+            if i == 0:
+                wiki_content = wikipedia.page(wikipedia.search(str(pred_class[0][i][1])[0])).content
+                results.append('{'+'Description of : '+ pred_class[0][i][1] + ',' + wiki_content[0:wiki_content.find(".")]+'}')
+        
+        os.remove(file_path)
+        
+        return " " .join(results)
     return None
-
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
